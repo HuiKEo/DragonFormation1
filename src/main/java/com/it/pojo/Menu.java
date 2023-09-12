495dc19743675bb70ff0cbfccab942f485931a2f
HuiKEo <86936695+HuiKEo@users.noreply.github.com>
2023-09-12 10:17:20 +0800

    生成器生成menuMapper

---
+++ src/main/java/com/it/pojo/Menu.java
@@ -0,0 +1,53 @@
+package com.it.pojo;
+
+public class Menu {
+    private Integer id;
+
+    private String menu;
+
+    private String path;
+
+    private Integer parentid;
+
+    private String icon;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu == null ? null : menu.trim();
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path == null ? null : path.trim();
+    }
+
+    public Integer getParentid() {
+        return parentid;
+    }
+
+    public void setParentid(Integer parentid) {
+        this.parentid = parentid;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon == null ? null : icon.trim();
+    }
+}
---
+++ src/test/java/com/it/Generator.java
@@ -0,0 +1,26 @@
+package com.it;
+
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generator {
+
+    @Test
+    public void testMbg() throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        InputStream configFile = Generator.class.getClassLoader().getResourceAsStream("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
