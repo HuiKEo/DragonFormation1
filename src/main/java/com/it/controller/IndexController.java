6d7b7cd4020c01eefaed758176cf6c63fef06604
ChenL <ambition-91@qq.com>
2023-09-19 09:09:37 +0800

    0919 权限控制

--- src/main/java/com/it/controller/IndexController.java
+++ src/main/java/com/it/controller/IndexController.java
@@ -7,25 +7,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Author Chen
  * @Date 2023/9/11 16:46
  */
-@Controller
+@RestController
 public class IndexController {
 
     @Autowired
     private MenuService menuService;
 
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String clientIndex(Model model){
-
+    @RequestMapping(value = "/getMenu",method = RequestMethod.GET)
+    public List<Menu> getMenu(){
         List<Menu> menuList  = menuService.getAuditMenu();
-        model.addAttribute("menuList",menuList);
-
-        return "client_index";
+//        model.addAttribute("menuList",menuList);
+        System.out.println(menuList);
+        return menuList;
     }
 }
