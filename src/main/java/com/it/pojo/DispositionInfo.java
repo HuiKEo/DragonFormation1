52e837424075574ed6b5ac8f6b82224e49fb1914
ywbhj <1716091607@qq.com>
2023-09-22 17:57:05 +0800

    HJ

---
+++ src/main/java/com/it/pojo/DispositionInfo.java
@@ -0,0 +1,65 @@
+package com.it.pojo;
+
+import java.util.Date;
+
+public class DispositionInfo {
+    private Integer id;
+
+    private String name;
+
+    private String licence;
+
+    private Integer status;
+
+    private Date createTime;
+
+    private Integer isdeleted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence == null ? null : licence.trim();
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getIsdeleted() {
+        return isdeleted;
+    }
+
+    public void setIsdeleted(Integer isdeleted) {
+        this.isdeleted = isdeleted;
+    }
+}
