7de55f826ff55275e4cd35dc4246f00ebb4af793
HuiKEo <86936695+HuiKEo@users.noreply.github.com>
2023-09-25 16:34:24 +0800

    9/25 Waste展示数据与分页

--- src/main/java/com/it/controller/WasteInfoController.java
+++ src/main/java/com/it/controller/WasteInfoController.java
@@ -1,10 +1,14 @@
 package com.it.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.it.models.PageSize;
 import com.it.pojo.WasteInfo;
 import com.it.service.WasteInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -17,9 +21,13 @@ public class WasteInfoController {
     public WasteInfoService wasteInfoService;
 
     @RequestMapping("/list")
-    public String findWasteAll(WasteInfo wasteInfo, HttpServletRequest request){
+    public String findWasteAll(WasteInfo wasteInfo, HttpServletRequest request,
+                               @RequestParam(defaultValue ="1") int pageNum){
+
+        PageHelper.startPage(pageNum, PageSize.PAGESIZE);
         List<WasteInfo> all = this.wasteInfoService.findAll(wasteInfo);
-        request.setAttribute("all",all);
+        PageInfo<WasteInfo> pageInfo =new PageInfo<>(all);
+        request.setAttribute("pageInfo",pageInfo);
         return "waste_info_list";
     }
 
